dc163cd937c3acf453a204e9020b286936a965cf
David Monarrez and Joe Pritchett <david@>
2019-01-14 11:51:42 -0800

    cooking app

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -1,8 +1,42 @@
-function add(num1, num2) {
-  return num1 + num2
+// function add(num1, num2) {
+//   return num1 + num2
+// }
+//
+// function sub(num1, num2) {
+//   return num1 - num2
+// }
+//
+// function mult(num1, num2) {
+//   return num1 * num2
+// }
+//
+// function div(num1, num2) {
+//   return num1 / num2
+// }
+//
+// var number1 = parseInt(prompt("Enter a number"));
+// var number2 = parseInt(prompt("Enter another number"));
+//
+//
+//
+// alert(div(number1, number2));
+
+
+//BMI
+// var height = parseInt(prompt("Enter height in inches"));
+// var weight = parseInt(prompt("weight in lbs"));
+//
+// function bmi(height, weight) {
+//   var userBMI = 703 * (weight / Math.pow(height, 2));
+//   return userBMI
+// }
+//alert(bmi(height, weight));
+
+var celsius = parseInt(prompt("Enter temp in celsius"));
+
+function  convert(celsius)  {
+  var farenheit=(celsius * (9/5)) + 32;
+  return farenheit
 }
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-
-alert(add(number1, number2));
+alert(convert(celsius));
---
+++ cooking-app/js/scripts.js
@@ -0,0 +1,8 @@
+var gallons = parseInt(prompt('Enter number of Gallons'));
+debugger
+function gal2Lit(gals) {
+  var lits = gals * 3.78541;
+  return lits
+}
+
+alert(gal2Lit(gallons));
